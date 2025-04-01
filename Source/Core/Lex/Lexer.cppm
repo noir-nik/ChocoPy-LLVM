e3faa21830ba7dbc8e3e2ec465e630487e210e7e
nm <niki8fw@gmail.com>
2025-04-01 18:35:17 +0300

    Add file reading

---
+++ Source/Core/Lex/Lexer.cppm
@@ -0,0 +1,7 @@
+export module Lexer;
+import std;
+
+export class Lexer {
+private:
+	
+};
--- Source/main.cpp
+++ Source/main.cpp
@@ -1,9 +1,19 @@
 #include <llvm/IR/LLVMContext.h>
 import std;
+import FileIOUtils;
 
 static llvm::LLVMContext TheContext;
 
 int main(int argc, char* argv[]) {
-	std::printf("Hello, World!\n");
+	// Lexer lexer;
+
+	auto content = Utils::ReadFile("Source/main.cpp");
+	if (!content) {
+		std::printf("Failed to read file\n");
+		return -1;
+	}
+
+	std::printf("%s\n", content->c_str());
+	
 	return 0;
 }
