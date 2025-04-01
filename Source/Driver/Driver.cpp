8f025a7a2ce8064bb23e55af0d2bda675d910b88
nm <niki8fw@gmail.com>
2025-04-01 19:09:40 +0300

    Build as library modules

--- Source/Driver/Driver.cpp
+++ Source/Driver/Driver.cpp
@@ -5,15 +5,13 @@ import FileIOUtils;
 static llvm::LLVMContext TheContext;
 
 int main(int argc, char* argv[]) {
-	// Lexer lexer;
-
-	auto content = Utils::ReadFile("Source/main.cpp");
+	auto content = Utils::ReadFile("Source/Driver/Driver.cpp");
 	if (!content) {
 		std::printf("Failed to read file\n");
 		return -1;
 	}
 
 	std::printf("%s\n", content->c_str());
-	
+	 
 	return 0;
 }
